948d4cce68bcd209bcd8a4610ab430d3a2f0676c
Scott Orr <AzureAD+ScottOrr@INC0854>
2021-06-11 14:10:44 -0400

    Refactored to parse MSWord XML files directly in order to retain formatting.

--- src/word-2-excel.py
+++ src/word-2-excel.py
@@ -1,88 +1,68 @@
 import csv
 from itertools import permutations
-from datetime import datetime
-import sys
 import re
+import sys
+from zipfile import ZipFile
+import datetime
 
-import docx2txt
-
-from convert_am_pm import convert24
+import lxml.etree as ET
 
 
-#
-# Create a simple .csv file
-#
+# Set constants.
+KEYWORDS = [
+            'Opened',
+            'Acknowledged',
+            'Dispatched',
+            'In Progress',
+            'Change Request',
+            'Suspended',
+            'Repaired',
+            'Completed',
+            'Closed',
+            'Rejected',
+            'Cancelled',
+            'Reopened'
+           ]
+time_format = "%m/%d/%y %I:%M %p"
 
-def create_excel_file(filename, sortedArray):
+
+def create_excel_file(filename, sorted_list):
+    """
+    Create a simple .csv file
+
+    """
 
     filename = filename.replace('docx','csv')
     with open(filename, mode='w', newline='') as _file:
-        _writer = csv.writer(_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        _writer = csv.writer(_file, delimiter=',', quotechar='"',
+                             quoting=csv.QUOTE_MINIMAL)
         _writer.writerow(['From Status', 'To Status', 'Effective Time'])
 
-        for _cell in sortedArray:
-            _writer.writerow([_cell[1], _cell[2], _cell[3]])
+        for _cell in sorted_list:
+            _writer.writerow([_cell[0], _cell[1],
+                              _cell[2].strftime(time_format)])
 
-
-#
-# Make a list of all possible 2 combo keywords
-#
 
 def make_permutations():
+    """
+    Make a list of all possible 2 combo keywords
+
+    """
+
     results = []
 
-    #keywords = ['Change Request', 'Suspended', 'In Progress', 'Acknowledged', 'Dispatched', 'Opened']
-    keywords = [
-                    'Opened',
-                    'Acknowledged',
-                    'Dispatched',
-                    'In Progress',
-                    'Change Request',
-                    'Suspended',
-                    'Repaired',
-                    'Completed',
-                    'Closed',
-                    'Rejected',
-                    'Cancelled',
-                    'Reopened'
-               ]
-
     # Get all permutations of length 2
-    perm = permutations(keywords, 2)
+    perms = permutations(KEYWORDS, 2)
 
     # Make the possible clauses
-    for _list in list(perm):
+    for _list in list(perms):
         search_term = "from " + _list[0] + " to " + _list[1]
         results.append(search_term)
 
     # Hard code in a 'to Opened'.  This is the starting state.
     results.append('to Opened')
+
     return results
-
-
-def find_clause_index(my_text, substring, start):
-    nlf = '\n'
-    _idx = my_text.find(substring, start)
-    result = ""
-
-    # We found a clause
-    if _idx != -1:
-
-        # Find the next newline character starting at the index we found our clause at
-        index_of_nextline = my_text.find(nlf, _idx)
-
-        # Find the one after that. We stop when we find a line that contains AM and/or PM
-
-        not_found = True
-        while not_found:
-            result = my_text[_idx:index_of_nextline]
-            if (result.find("AM") != -1 or result.find("PM") != -1) and lineStartsWithADate(result):
-                not_found = False
-            else:
-                _idx = index_of_nextline
-                index_of_nextline = my_text.find(nlf, _idx + 2)
-
-    return _idx, result
 
 
 def lineStartsWithADate(line):
@@ -99,6 +79,8 @@ def lineStartsWithADate(line):
 
 if __name__ == "__main__":
 
+
+    # Get the status change permutations.
     perms = make_permutations()
 
     # Attempt to find these substrings in our file.
@@ -116,64 +98,79 @@ if __name__ == "__main__":
 
     # Try getting the text
     try:
-        my_text = docx2txt.process(filename)
+        word = ZipFile(filename)
     except Exception as e:
-        print("Error extracting text from document: " + filename)   
+        print("Error opening Word document: " + filename)
         print(e)
-        sys.exit() 
+        raise
 
-    for perm in perms:
-        not_eof = True
-        previous_index = 0
-        while not_eof:
-            idx, snippet = find_clause_index(my_text, perm, previous_index)
-            if idx != -1:
-                # This perm was found in the file.  We will store this index & process this entry
-                # remembering that this perm may occur multiple times in the file.
-                previous_index = idx
+    # Read the document body into an XML Element object, and store the
+    # document namespace map.
+    document_root= ET.fromstring(word.read("word/document.xml"))
+    ns = document_root.nsmap
+    body = document_root.find("w:body", namespaces = ns)
 
-                idx = snippet.find('(effective')
-                if idx != -1:
-                    augment = len('(effective')
-                    paren = snippet.find(')')
-                    snippet = snippet[idx+augment:paren].strip()
+    # Find the title for the "Updates" table, then search through the table
+    # itself (which will be the next child element) and iterate through its
+    # rows to locate status changes.
 
-                if 'PM' in snippet:
-                    snippet = snippet.replace(' PM', ':00 PM')
+    updates_found = False
+    for child in body:
 
-                if 'AM' in snippet:
-                    snippet = snippet.replace(' AM', ':00 AM')
+        # Check to see if the previous child was the "Updates" title. If it
+        # is, search the table for updates, and then break the loop.
 
-                _snippet_list = snippet.split(" ", 1)
-                tmp = convert24(_snippet_list[1].strip())
-                _snippet_list[1] = tmp
+        if updates_found:
 
-                string_date = ' '.join(_snippet_list)
-                string_date = string_date.replace('\n', '')
-                string_date = string_date.strip()
+            # Find all table cells that match each of the status change
+            # permutations.
 
-                # Convert String ( ‘DD/MM/YY HH:MM:SS ‘) to datetime object
-                datetime_obj = datetime.strptime(string_date, '%m/%d/%y %H:%M:%S')
+            for perm in perms:
 
-                perm = perm.strip()
+                # Find all matching cells (if any).
+                query = './/w:tc[descendant::*[contains(text(), "' +  \
+                        perm + '")]]'
+                matches = child.xpath(query, namespaces = ns)
 
-                if "from " not in perm:
-                    frompart = "None"
-                    topart = perm[perm.index("to ") + 3:]
-                else:
-                    frompart = perm[5:perm.index(" to ")]
-                    topart = perm[perm.index(" to ") + 4:]
-                
-                d = [datetime_obj, str(frompart), str(topart), string_date]
-                final_list.append(d)
+                # If there are no matches--which will be true in the vast
+                # majority of cases--this will be an empty loop.
+                for match in matches:
 
-            else:
-                not_eof = False
+                    # The time is always the last paragraph in an update
+                    # cell. However, this one-line paragraph is sometimes
+                    # split across two (or possibly more) text elements.
+                    time_para = match[-1].findall(".//w:t", namespaces = ns)
+                    time_para_string = "".join([string.text for string
+                                                in time_para])
 
-    sortedArray = sorted(
-        final_list,
-        key=lambda x: datetime.strptime(x[3], '%m/%d/%y %H:%M:%S'), reverse=False
-    )
+                    # Pull out the effective time, discarding the time of
+                    # update entry, if any (there's usually an entry time,
+                    # but not always--hence we pull out the last sub-
+                    # string).
+                    time_string = \
+                        time_para_string.split("effective")[-1].strip(" )")
+                    time = datetime.datetime.strptime(time_string, time_format)
 
-    create_excel_file(filename, sortedArray)
+                    if perm.startswith("to "):
+                        from_part = "None"
+                        to_part = perm.split("to ")[1]
+                    else:
+                        perm_split = perm.split(" to ")
+                        from_part = perm_split[0].split("from ")[1]
+                        to_part = perm_split[1]
+
+                    d = [from_part, to_part, time]
+                    final_list.append(d)
+
+            # If we've found and searched the "Updates" table, we're done.
+            break
+
+        # Unless the "Updates" title is found, this will be an empty list,
+        # which will evaluate as False in the Boolean test at the beginning
+        # of the loop.
+        updates_found = child.xpath('.//w:t[text()="Updates"]', namespaces = ns)
+
+    sorted_list = sorted(final_list, key = lambda x: x[2], reverse = False)
+
+    create_excel_file(filename, sorted_list)
     print("\nRun completed.")
